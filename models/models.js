aa7280860a1ef2048119116d5e07a4d3c5f2413f
Diego Sanz <diegolangos@gmail.com>
2015-07-14 19:15:14 +0200

    Agregar sequelize (Mod 7, tema 2)

---
+++ models/models.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar DB SQLite
+var sequelize = new Sequelize(null, null, null, {
+	dialect: 'sqlite',
+	storage: 'quiz.sqlite'
+});
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+// exportar la definición de la tabla Quiz
+exports.Quiz = Quiz;
+
+// sequelize.sync() crea e inicializa la tabla de preguntas en DB
+sequelize.sync().success(function(){
+	// success() ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function(count){
+		// la tabla se inicializa solo si está vacía
+		if(count === 0){
+			Quiz.create({
+				pregunta: 'Capital de Italia',
+				respuesta : 'Roma'
+			})
+			// FIXME: esto no va a funcionar por la versión de sequelize, mirar promises
+			.success(function(){
+				console.log('Base de datos inicializada');
+			});
+		}
+	});
+});
