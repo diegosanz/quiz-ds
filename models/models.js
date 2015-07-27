aa868b853cfa80d8bf832e9ee5c7f1be6144928d
Diego Sanz <diegolangos@gmail.com>
2015-07-27 22:12:17 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -3,13 +3,13 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite	 DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6]||null);
-var user = (url[2]||null);
-var pwd = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect = (url[1]||null);
-var port = (url[5]||null);
-var host = (url[4]||null);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
@@ -32,16 +32,15 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().then(function(){
-	// then() ejecuta el manejador una vez creada la tabla
-	Quiz.count().then(function(count){
+sequelize.sync().then(function() {
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function (count){
 		// la tabla se inicializa solo si está vacía
-		if(count === 0){
-			Quiz.create({
-				pregunta: 'Capital de Italia',
-				respuesta : 'Roma'
-			})
-			// FIXME: esto no va a funcionar por la versión de sequelize, mirar promises
+		if(count === 0) {
+			Quiz.bulkCreate([
+				{pregunta: 'Capital de Italia', respuesta: 'Roma'},
+				{pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+			])
 			.then(function(){
 				console.log('Base de datos inicializada');
 			});
