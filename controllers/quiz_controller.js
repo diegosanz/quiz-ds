7362cf7f6c3a3b422f517c24a035c98e72b341e9
Diego Sanz <diegolangos@gmail.com>
2015-07-29 18:56:13 +0200

    Añadir filtro búsquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,33 +3,46 @@ var models = require('../models/models.js');
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId).then(
-		function(quiz){
-			if(quiz){
+		function(quiz) {
+			if (quiz) {
 				req.quiz = quiz;
 				next();
-			}else{
-				next( new Error('No existe quizId=' + quizId) );
+			} else {
+				next(new Error('No existe quizId=' + quizId));
 			}
 		}
-	).catch(function(error) { next(error);});
-};
-
-// GET /quizes
-exports.index = function(req, res){
-	models.Quiz.findAll()
-	.then(
-		function(quizes){
-			res.render('quizes/index', { quizes: quizes });
-		}
-	)
-	.catch(function(error){
+	).catch(function(error) {
 		next(error);
 	});
 };
 
+// GET /quizes
+exports.index = function(req, res) {
+
+	var filter = {};
+	if (req.query.search !== undefined) {
+		filter.where = ["pregunta like ?", "%" + req.query.search.split(" ").join("%") + "%"];
+	}
+
+	models.Quiz
+		.findAll(filter)
+		.then(
+			function(quizes) {
+				res.render('quizes/index', {
+					quizes: quizes
+				});
+			}
+		)
+		.catch(function(error) {
+			next(error);
+		});
+};
+
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', { quiz: req.quiz });
+	res.render('quizes/show', {
+		quiz: req.quiz
+	});
 };
 
 // GET /quizes/:id/answer
--- models/models.js
+++ models/models.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite	 DATABASE_URL = sqlite://:@:/
+// SQLite DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6] || null);
 var user = (url[2] || null);
@@ -16,7 +16,7 @@ var storage = process.env.DATABASE_STORAGE;
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
-var sequelize = new Sequelize(DB_name, user, pwd,{
+var sequelize = new Sequelize(DB_name, user, pwd, {
 	dialect: protocol,
 	protocol: protocol,
 	port: port,
@@ -34,16 +34,19 @@ exports.Quiz = Quiz;
 // sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().then(function() {
 	// then(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().then(function (count){
+	Quiz.count().then(function(count) {
 		// la tabla se inicializa solo si está vacía
-		if(count === 0) {
-			Quiz.bulkCreate([
-				{pregunta: 'Capital de Italia', respuesta: 'Roma'},
-				{pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
-			])
-			.then(function(){
-				console.log('Base de datos inicializada');
-			});
+		if (count === 0) {
+			Quiz.bulkCreate([{
+					pregunta: 'Capital de Italia',
+					respuesta: 'Roma'
+				}, {
+					pregunta: 'Capital de Portugal',
+					respuesta: 'Lisboa'
+				}])
+				.then(function() {
+					console.log('Base de datos inicializada');
+				});
 		}
 	});
 });
